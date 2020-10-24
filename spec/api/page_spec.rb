26c881f4c878b6214a384492e98569b026241f16
Iain McNulty <iain@inulty.com>
2020-10-24 11:27:48 +0100

    Move page cassettes to pages directory

--- spec/api/page_spec.rb
+++ spec/api/page_spec.rb
@@ -5,7 +5,7 @@ require 'spec_helper'
 RSpec.describe Spage::Page do
   describe '#all' do
     it 'parses all fields correctly' do
-      VCR.use_cassette('all_pages') do
+      VCR.use_cassette('pages/all') do
         pages = Spage::Api::Page.new.all
 
         expect(pages.first.id).to eq('hmw075ww7tlq')
@@ -15,7 +15,7 @@ RSpec.describe Spage::Page do
 
   describe '#find' do
     it 'parses all fields correctly' do
-      VCR.use_cassette('find_page') do
+      VCR.use_cassette('pages/find') do
         page = Spage::Api::Page.new.find('hmw075ww7tlq')
         expect(page.updated_at).to be_a(DateTime)
       end
@@ -25,14 +25,14 @@ RSpec.describe Spage::Page do
   describe '#update' do
     context '200 page is updated' do
       it 'updates the url' do
-        page = VCR.use_cassette('find_page') do
+        page = VCR.use_cassette('pages/find') do
           Spage::Api::Page.new.find('hmw075ww7tlq')
         end
         expect(page.url).to be_nil
 
         page.url = 'https://example.com'
 
-        VCR.use_cassette('update_page_200_page_is_updated') do
+        VCR.use_cassette('pages/update_200_page_is_updated') do
           updated_page = Spage::Api::Page.new.update('hmw075ww7tlq', page)
           expect(updated_page.url).to eq('https://example.com')
         end
@@ -41,14 +41,14 @@ RSpec.describe Spage::Page do
 
     context '400 page is missing' do
       it 'raises error when body has no "page" root property' do
-        page = VCR.use_cassette('find_page') do
+        page = VCR.use_cassette('pages/find') do
           Spage::Api::Page.new.find('hmw075ww7tlq')
         end
         expect(page.url).to be_nil
 
         page.url = 'https://example.com'
 
-        VCR.use_cassette('update_page_400_page_is_missing') do
+        VCR.use_cassette('pages/update_400_page_is_missing') do
           expect {
             Spage::Api::Page.new.update('hmw075ww7tlq', page)
           }.to raise_error(Spage::Error, /page is missing/)
--- spec/resources/page_spec.rb
+++ spec/resources/page_spec.rb
@@ -5,7 +5,7 @@ require 'spec_helper'
 RSpec.describe Spage::Page do
   describe 'builds a model from json' do
     it 'parses all fields correctly' do
-      VCR.use_cassette('all_pages') do
+      VCR.use_cassette('pages/all') do
         pages = Spage::Api::Page.new.all
         expect(pages.first).to be_a Spage::Page
       end
